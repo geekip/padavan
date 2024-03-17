446b63db0daace768ea274859e95390e3d7337b0
geekip <mail@yangfei.site>
2024-03-17 14:36:23 +0800

    Create Project

---
+++ DIY-RM2100.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 路由器ip
+sed -i 's/192.168.2.1"/10.0.0.1"/' trunk/user/shared/defaults.h
+# DHCP开始ip
+sed -i 's/192.168.2.100/10.0.0.11/' trunk/user/shared/defaults.h
+# DHCP结束ip
+sed -i 's/192.168.2.244/10.0.0.244/' trunk/user/shared/defaults.h
